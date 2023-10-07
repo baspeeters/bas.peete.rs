1f876f824953200c849123735c45ac40c76fa8f0
Bas Peeters <bas@peete.rs>
2023-10-07 19:17:56 +0200

    Remove Google Tag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  runtimeConfig: {
-    public: {
-      gtagId: "UA-100999309-2",
-    },
-  },
 })
