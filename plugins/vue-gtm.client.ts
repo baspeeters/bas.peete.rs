6dc62b2f942410e46f0db660eac3f79ed7efe800
Bas Peeters <bas@peete.rs>
2023-10-07 19:13:41 +0200

    Always add Google Tag

--- plugins/vue-gtm.client.ts
+++ plugins/vue-gtm.client.ts
@@ -1,7 +1,6 @@
 import { createGtm } from "@gtm-support/vue-gtm";
 
 export default defineNuxtPlugin((nuxtApp) => {
-    if (useRuntimeConfig().public.appEnv !== 'production') return
     nuxtApp.vueApp.use(createGtm({
         id: 'G-N7X8SPBLHB',
         defer: false,
