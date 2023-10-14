df97fb627c74ce285acf0d233e0065e59d05fda6
Bas Peeters <bas@peete.rs>
2023-10-14 20:02:09 +0200

    Wrap article around about-me page structured data

--- pages/about-me.vue
+++ pages/about-me.vue
@@ -1,11 +1,18 @@
 <script lang="ts" setup>
 import me from "~/lib/me";
+import { Article } from "schema-dts";
 
 definePageMeta({
   title: "About me",
 });
 
-useNuxtApp().$structuredData(me);
+useNuxtApp().$structuredData(<Article>{
+  "@context": "https://schema.org/",
+  "@type": "Article",
+  headline: "About me",
+  image: "https://1.gravatar.com/avatar/9d4f09a9a169d0d6dddd4d0980cc94be?size=256",  author: me,
+  about: me,
+});
 </script>
 
 <template>
--- plugins/structured-data.client.ts
+++ plugins/structured-data.client.ts
@@ -4,7 +4,6 @@ export default defineNuxtPlugin(() => {
     return {
         provide: {
             structuredData: (thing: Thing) => {
-                console.log("bla", thing);
                 useHead({
                     script: [{
                         type: "application/ld+json",
