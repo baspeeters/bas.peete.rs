43767f6f94c1e6c2bfec310d27587f91c94ff8a3
Bas Peeters <bas@peete.rs>
2024-03-02 18:38:54 +0100

    chore: Add GA4 tag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      script: [
+        {
+          async: true,
+          src: "https://www.googletagmanager.com/gtag/js?id=UA-38936174-1",
+        },
+        {
+          children: `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'GTM-WXZM4SW3');`,
+        },
+      ],
+    },
+  },
   css: ["~/assets/css/main.scss"],
   devtools: { enabled: true },
   ssr: false,
